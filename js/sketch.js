a45512b64c4d10756ac4830972b28ee001ca9173
andreacmozo <andrea@Billys-MacBook-Pro-2.local>
2017-06-21 13:21:00 -0400

    Updates v1.2
    This updated includes background image, and title of game.

--- js/sketch.js
+++ js/sketch.js
@@ -1,37 +1,59 @@
 var pig;
 var pigImg;
+var bg;
+var leveltitle = "";
 
 function preload() {
+//Load all images
 pigImg = loadImage("images/donaldthepig.png");
+bg = loadImage("images/background.png");
 }
 
 function setup() {
+  //Add all images
+  //use console.log () to debug
   createCanvas(1000,400);
 
-  pig = createSprite (200, 200, width/2, height/2);
+  pig = createSprite (110, 280, width/2, height/2);
   pig.addImage(pigImg)
+
 }
 
 function draw() {
-  background(211,211,211);
+
+  background(bg);
+
+  //anytime you want ot modify text, it has to go before text
+  fill ("red");
+  textSize (32);
+  text("Alternative World", 10, 30);
+
   drawSprites();
 
-  if (pig.overlapPixel(1300,200)){
-   pig.position.x = 200;
-   pig.position.y = 200;
-   pig.setSpeed(0, 0);
-    }
+  if (pig.position.x >= 1200){
+    pig.position.x = 110;
+    pig.position.y = 280;
+    pig.setSpeed(0, 0);
   }
+ }
 
 function keyPressed() {
   if (keyCode == RIGHT_ARROW) {
-    pig.setSpeed(1.5, 0);
+    pig.setSpeed(3.0, 0);
   }
   else if (keyCode == LEFT_ARROW) {
-    pig.setSpeed(1.5, 180);
+    pig.setSpeed(3.0, 180);
   }
   else if (key == ' ') {
     pig.setSpeed(0, 0);
   }
   return false;
+}
+
+function level1 () {
+
+}
+
+function level2 () {
+
 }
