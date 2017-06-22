200010e25d9bdb96629fecbc79c677b73056021a
andreacmozo <andrea@Billys-MacBook-Pro-2.local>
2017-06-21 20:13:07 -0400

    Animation
    Loaded and added pig animation

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,9 @@
 var pig;
-var pigImg;
 var bg;
 var leveltitle = "";
 
 function preload() {
-//Load all images
-pigImg = loadImage("images/donaldthepig.png");
+
 bg = loadImage("images/background.png");
 }
 
@@ -14,13 +12,12 @@ function setup() {
   //use console.log () to debug
   createCanvas(1000,400);
 
-  pig = createSprite (110, 280, width/2, height/2);
-  pig.addImage(pigImg)
-
+  pig = createSprite(110, 280, width/2, height/2);
+  pig.addAnimation("normal", "images/sprite_0.png", "images/sprite_1.png", "images/sprite_2.png", "images/sprite_3.png", "images/sprite_4.png")
+  pig.looping = false;
 }
 
 function draw() {
-
   background(bg);
 
   //anytime you want ot modify text, it has to go before text
@@ -28,27 +25,29 @@ function draw() {
   textSize (32);
   text("Alternative World", 10, 30);
 
-  drawSprites();
+  drawSprites ();
 
-  if (pig.position.x >= 1200){
-    pig.position.x = 110;
-    pig.position.y = 280;
-    pig.setSpeed(0, 0);
-  }
+  animation(pig, 110, 280);
+
+if (pig.position.x >= 1200){
+  pig.position.x = 110;
+  pig.position.y = 280;
+  pig.setSpeed(0, 0);
  }
+}
 
 function keyPressed() {
-  if (keyCode == RIGHT_ARROW) {
-    pig.setSpeed(3.0, 0);
-  }
-  else if (keyCode == LEFT_ARROW) {
-    pig.setSpeed(3.0, 180);
-  }
-  else if (key == ' ') {
-    pig.setSpeed(0, 0);
-  }
-  return false;
-}
+ if (keyCode == RIGHT_ARROW) {
+   pig.setSpeed(3.0, 0);
+ }
+ else if (keyCode == LEFT_ARROW) {
+   pig.setSpeed(3.0, 180);
+ }
+ else if (key == ' ') {
+   pig.setSpeed(0, 0);
+ }
+ return false;
+ }
 
 function level1 () {
 
